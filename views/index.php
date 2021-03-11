1023f9cfaa198ea08dae6f03cadf5bbb926a8a78
RenanMetzen <55636616+RenanMetzen@users.noreply.github.com>
2021-03-11 16:55:47 -0300

    ok

--- views/index.php
+++ views/index.php
@@ -4,56 +4,64 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="padding:0; min-height: 100vh; box-sizing: border-box; margin: 0;">
 <head>
   <title>API Despesas</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Cálculo de Kcalorias basais</a>
+<body style="padding:0; min-height: 100vh; box-sizing: border-box; margin: 0;">
+<div style="display:flex; height:100%; flex-direction:column; justify-content:space-between;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+      <h4>Cálculo de Kcalorias basais</h4>
 </nav>
-<div class="form-row col-md-12">
-  <div class="form-group col-md-4"></div>
-    <div class="form-group col-md-4" style="display:flex; flex-direction:column; height:600px;justify-content:space-around; id="inputs">
-<h4><u>Cadastro:</u></h4>
-<div>
-      <label for="biotipo">Biotipo</label>
-      <select class="form-control" id="biotipo">
-        <option value=1>Endomorfo</option>
-        <option value=2>Mesomorfo</option>
-        <option value=3>Ectomorfo</option>
-      </select>
+    <div class="form-group col-md-4 container-fluid" style="padding:10; height:100%; border-bottom-style: solid;">
+      <h3 style="text-align:center;"><b>Cadastro:</b></h3>
+      <div class="form-group">
+        <label for="biotipo">Biotipo</label>
+        <select class="form-control" id="biotipo">
+          <option value=0 disabled selected>Selecione o seu biotipo</option>
+          <option value=1>Endomorfo</option>
+          <option value=2>Mesomorfo</option>
+          <option value=3>Ectomorfo</option>
+        </select>
       </div>
 
-      <div>
-      <label for="altura">Altura(cm)</label>
-      <input class="form-control" id="altura" type="number" name="altura">
+      <div class="form-group">
+        <label for="altura">Altura(cm)</label>
+        <input class="form-control" id="altura" type="number" name="altura" placeholder="0">
       </div>
     
-      <div>
-      <label for="idade">Idade</label>
-      <input class="form-control" id="idade" type="number" name="idade">
+      <div class="form-group">
+        <label for="idade">Idade</label>
+        <input class="form-control" id="idade" type="number" name="idade" placeholder="0">
       </div>
 
-      <div>
-      <label for="peso">Peso(g)</label>
-      <input class="form-control" id="peso" type="number" name="peso">
+      <div class="form-group">
+        <label for="peso">Peso(g)</label>
+        <input class="form-control" id="peso" type="number" name="peso" placeholder="0">
       </div>
 
-      <div>
-      <label for="atividade">Atividade Física</label>
-      <select class="form-control" id="atividade">
-        <option value=1>Sedentário</option>
-        <option value=2>Moderada</option>
-        <option value=3>Alta</option>
-        <option value=4>Extrema</option>
-      </select>
+      <div class="form-group">
+        <label for="atividade">Atividade Física</label>
+        <select class="form-control" id="atividade">
+          <option value=0 disabled selected>Selecione a sua frequência de atividade física</option>
+          <option value=1>Sedentário</option>
+          <option value=2>Moderada</option>
+          <option value=3>Alta</option>
+          <option value=4>Extrema</option>
+        </select>
       </div>
+
+      <button type="button" class="btn btn-primary float-right">Cadastrar</button>
+      
     </div>
+    
+  <footer class="bg-dark text-center text-white" style="background-color: #9933CC; width:100%;">
+    <p>Author: Hege Refsnes</p>
+    <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+  </footer>
   </div>
-</div>
-<div class="form-group col-md-4"></div>
-</div>
+</body>
 <script src='js/main.js'></script>
 </html>
